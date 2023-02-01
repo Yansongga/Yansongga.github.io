81f040c2d41d47e9d99bcd91ce2ae218d3b5237f
Yansongga <54696448+Yansongga@users.noreply.github.com>
2023-02-01 12:07:20 +0800

    Create 15-ReadData.py

---
+++ leets_lib/15-ReadData.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    p = int(first_multiple_input[1])
+
+    astronaut = []
+
+    for _ in range(p):
+        astronaut.append(list(map(int, input().rstrip().split())))
+
+    result = journeyToMoon(n, astronaut)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
